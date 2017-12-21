a41003d302fd248a703ae02bb13f1536b18828ba
wemlion <angusfu1126@qq.com>
2017-12-21 21:41:58 +0800

    update

--- index.js
+++ index.js
@@ -1,44 +1,24 @@
-var through = require('through')
-var md2vue = require('md2vue')
+const md2vue = require('md2vue')
+const loaderUtils = require('loader-utils')
 
-module.exports = function mdvueify (file, options) {
-  if (!/.md$/.test(file)) {
-    return through()
-  }
-  console.log(options)
+module.exports = function (content, map, meta) {
+  var callback = this.async()
+  var options = loaderUtils.getOptions(this) || {}
 
-  var data = ''
-  var stream = through(write, end)
-  stream.mdvueify = true
-
-  function write (buf) {
-    data += buf
-  }
-
-  function end () {
-    stream.emit('file', file)
-
-    var id = 1333
-    var customMarkups = () => {
-      var uid = 'vue-demo-' + (id++)
-      return '<input id="' + uid +'" type="checkbox" /><label for="' + uid +'"></label>'
-    }
-    
-    md2vue(data, {
-      target: 'js',
-      highlight: 'prism',
-      componentName: 'xxx',
-      customMarkups: customMarkups
-    })
-    .then(function (result) {
-      stream.queue(result)
-      stream.queue(null)
-    })
-    .catch(function (error) {
-      stream.emit('error', error)
-      console.error(error.stack.replace(/^.*?\n/, ''))
-    })
-  }
-
-  return stream
+  md2vue(content, {
+    target: 'js',
+    componentName: 'default',
+    shadow: !!options.shadow,
+    highlight: options.highlight || 'highlight.js'
+  })
+  .then(content => {
+    var ret = `module.exports = function() {
+      ${content}
+      return moduleExports;
+    }.call({})`
+    callback(null, ret, map, meta)
+  })
+  .catch(err => {
+    callback(err)
+  })
 }
